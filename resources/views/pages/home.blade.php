f73a8715c30fb8eb3fa470110970ce150ddd9d70
Ronnel <ron@24digital.com.au>
2016-06-24 23:08:07 +0800

    homepage layout

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,9 +1,56 @@
-@extends('layouts.master')
+@extends('layouts.landing')
 @section('content')
 
     <div class="container content">
-        <div class="row">
-                <div class="logo-container"><img src="img/logo.png" alt="" /></div>
+        <div class="row app-content">
+            <div class="small-4 columns download-option">
+                <p>Install FIGHTCALLOUT for IOS!</p>
+                <img src="img/appstore.png" alt="" />
+            </div>
+            <div class="small-4 columns">
+                <div class="logo-container">
+                    <img src="img/logo_home.png" alt="" />
+                </div>
+                <div class="callout-container">
+                    <img src="img/fco.png" alt="" />
+                </div>
+            </div>
+            <div class="small-4 columns download-option">
+                <p>Install FIGHTCALLOUT for Android!</p>
+                <img src="img/googleplay.png" alt="" />
+            </div>
+
+            <div class="small-12 columns">
+                <div class="socialize-container">
+                    <p>Socialize with us!</p>
+                    <ul>
+                        <li>
+                            <a href="#">
+                                <img src="img/fco_fb.png" alt="" />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src="img/fco_twitter.png" alt="" />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src="img/fco_mail.png" alt="" />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src="img/fco_google.png" alt="" />
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="small-12 columns footer">
+                <p>All Rights Reserved 2016</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
         </div>
     </div>
 
