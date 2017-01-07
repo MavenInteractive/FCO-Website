d5a81f172625ab0dbcb73421668575e8558b6902
Ronnel <ron@24digital.com.au>
2017-01-07 14:54:31 +0800

    callout

--- resources/views/pages/callout.blade.php
+++ resources/views/pages/callout.blade.php
@@ -46,7 +46,7 @@
                         Details
                     </div>
                     <div class="section-content">
-                        {{ $callout->description }} Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        {{ $callout->description }}
                     </div>
                     <div class="button-container">
                         <a href="#" id="showMoreDetails">Show More <i class="fa fa-angle-right" aria-hidden="true"></i></a>
